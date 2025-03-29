91c3822cd07299bb10eacff91b0da0b98946c98a
Xelzoid <zanggar.manarbek@icloud.com>
2025-03-29 18:18:21 +0500

    simulations list and theme

--- app/simulation/page.tsx
+++ app/simulation/page.tsx
@@ -1,100 +1,122 @@
-"use client"
-import { TypingAnimation } from "@/components/magicui/typing-animation";
-import { Button } from "@heroui/button";
-import { addToast } from "@heroui/toast";
-import { motion } from "framer-motion";
-import { useEffect, useRef, useState } from "react"
+"use client";
+import { useEffect, useState } from "react";
+import { Card, CardHeader, CardBody } from "@heroui/card";
+import { Divider } from "@heroui/divider";
+import { Pagination } from "@heroui/pagination";
+import { animate, motion } from "framer-motion";
+import { axiosInstance } from "@/lib/axios";
+import { CardContainer as HoverContainer, CardBody as HoverBody, CardItem as HoverItem } from "@/components/ui/3d-card";
+import { AnimatedCircularProgressBar } from "@/components/magicui/animated-circular-progress-bar";
+import { Link } from "@heroui/link";
 
-
-interface Message {
-    type: string;
-    content: string;
-    sender: string;
+interface Simulation {
+  id: number;
+  name: string;
+  description: string;
+  score: number
 }
 
-interface Action {
-    description: string;
-    id: number;
-}
+export default function VRPage() {
+  const [sims, setSims] = useState<Simulation[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  // how many sims per page
+  const pageSize = 15;
 
-function TextAnimate({ children, className }: { children: React.ReactNode, className?: string }) {
-    return (
-        <motion.div
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        className={className}
-        >
-            {children}
-        </motion.div>
-    )
+  // Dummy data for testing 
+  const fakeSims: Simulation[] = [];
+  for (let i = 1; i <= 100; i++) {
+    fakeSims.push({
+      id: i,
+      name: `Simulation ${i}`,
+      description: `Description for Simulation ${i}`,
+      score: Math.floor(Math.random() * 100),
+    });
+  }
+  // Calculate for fake data
+  const totalPages = Math.ceil(fakeSims.length / pageSize);
+  // delete above ^^
 
-}
+  useEffect(() => {
+    setLoading(true);
 
-export default function Page() {
-    const ws = useRef<WebSocket | null>(null);
-    const [messages, setMessages] = useState<Message[]>([]);
-    const [actions, setActions] = useState<Action[]>([]);
+    // Uncomment :
+    /*
+    axiosInstance.get(`/simulations/?page=${page}`).then((res) => {
+      setSims(res.data);
+      setLoading(false);
+    }).catch((err) => {
+      console.error(err);
+      setLoading(false);
+    });
+    */
 
-    useEffect(() => {
-        ws.current = new WebSocket("ws://localhost:8000/simulations/ws?id=1");
+    // fake data:
+    const startIndex = (page - 1) * pageSize;
+    const paginatedFiles = fakeSims.slice(startIndex, startIndex + pageSize);
+    setSims(paginatedFiles);
+    setLoading(false);
+  }, [page]);
 
-        ws.current.onmessage = (event) => {
-            const data = JSON.parse(event.data);
-            if (data.end) {
-                ws.current?.close();
-                addToast({ description: "Simulation ended",  });
-                return;
-            }
-            data.messages.forEach((message: any) => message.sender = "bot");
-            setMessages((prevMessages) => [...prevMessages, ...data.messages]);
-            setActions(data.actions);
-        }
-    }, []);
+  return (
+    <div className="w-full p-6 h-full translate-y-[-50px]">
+      <div className="max-w-6xl mx-auto h-full">
+        <h1 className="text-3xl font-bold text-center mb-6">simulations</h1>
 
-    return (
-        <>
-        <div className="w-full flex flex-col h-full rounded-lg bg-accent">
-            <div className="w-full flex flex-col max-h-full overflow-scroll">
-            {
-                messages.map((message, index) => {
-                    if (message.type === "action") return (
-                        <div className="mx-auto p-3 ">
-                            <TextAnimate key={index}>
-                            {message.content}
-                            </TextAnimate>                        
-                        </div>
-                    )
-
-                    if (message.sender === "bot") return (
-                        <div className="m-4 p-3 rounded-lg max-w-[75%] text-sm bg-blue-500">
-                            <TypingAnimation key={index} className="text-sm">
-                            {message.content}
-                            </TypingAnimation>
-                        </div>
-                    )
-
-                    return (
-                        <div className="m-4 p-3 rounded-lg max-w-[75%] text-sm bg-blue-500">
-                            <TextAnimate key={index} className="ml-auto">
-                            {message.content}
-                            </TextAnimate>
-                        </div>
-                    )
-                })
-            }
+        {loading ? (
+          <p className="text-center">Загрузка файлов...</p>
+        ) : (
+          <>
+            <div className="min-h-[50vh]">
+              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-6">
+                {sims.map((sim) => (
+                  <HoverContainer >
+                    <HoverBody key={sim.id}  >
+                      <HoverItem translateZ={40}>
+                        <motion.div 
+                          initial={{ opacity: 0, scale: 0.8 }}
+                          animate={{ opacity: 1, scale: 1 }}
+                          className="flex flex-col items-center justify-center min-h-10 rounded-lg"
+                        >
+                          <Card isPressable as={Link} href={`/simulation/${sim.id}`} className="rounded-xl shadow-lg p-2">
+                            <CardHeader className="">
+                              <p className="text-center text-md w-full">{sim.name}</p>
+                            </CardHeader>
+                            <Divider className=""/> 
+                            <CardBody className="flex flex-row w-full space-x-4 items-center justify-center">
+                              <p className="text-sm flex-grow-1">{sim.description}</p>
+                              <div className="flex flex-row space-x-4 items-center justify-center">
+                                <Divider orientation="vertical" className="h-7"></Divider>
+                                <AnimatedCircularProgressBar
+                                  max={100}
+                                  min={0}
+                                  value={sim.score}
+                                  gaugePrimaryColor={sim.id % 2 === 0 ? "#e22323" : "#438de1"}
+                                  gaugeSecondaryColor="rgba(0, 0, 0, 0.1)"
+                                />
+                              </div>
+                            </CardBody>
+                          </Card>
+                        </motion.div>
+                      </HoverItem>
+                    </HoverBody>
+                  </HoverContainer>
+                ))}
+              </div>
             </div>
-            <div className="flex justify-center">
-                {
-                    actions.map((action, index) => {
-                        return (
-                            <Button key={index} className="m-2 text-wrap" onPress={() => ws.current?.send(JSON.stringify({ id: action.id }))}>
-                                {action.description}
-                            </Button>
-                        )
-                    })
-                }
+            <div className="mt-6 flex justify-center">
+              <Pagination 
+                isCompact 
+                showControls 
+                initialPage={1} 
+                total={totalPages} 
+                page={page} 
+                onChange={setPage}
+              />
             </div>
-        </div>
-        </>
-    )
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -23,8 +23,8 @@ const config = {
   			sm: 'calc(var(--radius) - 4px)'
   		},
   		colors: {
-  			background: 'hsl(var(--background))',
-  			foreground: 'hsl(var(--foreground))',
+  			// background: 'hsl(var(--background))',
+  			// foreground: 'hsl(var(--foreground))',
   			card: {
   				DEFAULT: 'hsl(var(--card))',
   				foreground: 'hsl(var(--card-foreground))'
@@ -33,14 +33,14 @@ const config = {
   				DEFAULT: 'hsl(var(--popover))',
   				foreground: 'hsl(var(--popover-foreground))'
   			},
-  			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
-  				foreground: 'hsl(var(--primary-foreground))'
-  			},
-  			secondary: {
-  				DEFAULT: 'hsl(var(--secondary))',
-  				foreground: 'hsl(var(--secondary-foreground))'
-  			},
+  			// primary: {
+  			// 	DEFAULT: 'hsl(var(--primary))',
+  			// 	foreground: 'hsl(var(--primary-foreground))'
+  			// },
+  			// secondary: {
+  			// 	DEFAULT: 'hsl(var(--secondary))',
+  			// 	foreground: 'hsl(var(--secondary-foreground))'
+  			// },
   			muted: {
   				DEFAULT: 'hsl(var(--muted))',
   				foreground: 'hsl(var(--muted-foreground))'
@@ -69,23 +69,228 @@ const config = {
   darkMode: "class",
   plugins: [
     heroui({
-      themes: {
-        light: {
-          layout: {},
-          colors: {
-            primary: '#006FEE',
-            secondary: '#DF2935',
-          }
-        },
-        dark: {
-          layout: {},
-          colors: {
-            primary: '#DF2935',
-            secondary: '#006FEE',
-          }
-        },
-      }
-    }), require("tailwindcss-animate")
+		"themes": {
+		  "light": {
+			"colors": {
+			  "default": {
+				"50": "#fafafa",
+				"100": "#f2f2f3",
+				"200": "#ebebec",
+				"300": "#e3e3e6",
+				"400": "#dcdcdf",
+				"500": "#d4d4d8",
+				"600": "#afafb2",
+				"700": "#8a8a8c",
+				"800": "#656567",
+				"900": "#404041",
+				"foreground": "#000",
+				"DEFAULT": "#d4d4d8"
+			  },
+			  "primary": {
+				"50": "#e8f1fb",
+				"100": "#c7ddf6",
+				"200": "#a6c9f1",
+				"300": "#85b5ec",
+				"400": "#64a1e6",
+				"500": "#438de1",
+				"600": "#3774ba",
+				"700": "#2c5c92",
+				"800": "#20436b",
+				"900": "#142a44",
+				"foreground": "#000",
+				"DEFAULT": "#438de1"
+			  },
+			  "secondary": {
+				"50": "#fbe4e4",
+				"100": "#f6bdbd",
+				"200": "#f19797",
+				"300": "#ec7070",
+				"400": "#e74a4a",
+				"500": "#e22323",
+				"600": "#ba1d1d",
+				"700": "#931717",
+				"800": "#6b1111",
+				"900": "#440b0b",
+				"foreground": "#fff",
+				"DEFAULT": "#e22323"
+			  },
+			  "success": {
+				"50": "#e7f8df",
+				"100": "#c5edb3",
+				"200": "#a3e386",
+				"300": "#82d859",
+				"400": "#60ce2d",
+				"500": "#3ec300",
+				"600": "#33a100",
+				"700": "#287f00",
+				"800": "#1d5d00",
+				"900": "#133b00",
+				"foreground": "#000",
+				"DEFAULT": "#3ec300"
+			  },
+			  "warning": {
+				"50": "#fffcdf",
+				"100": "#fff8b3",
+				"200": "#fff586",
+				"300": "#fff159",
+				"400": "#ffed2d",
+				"500": "#ffe900",
+				"600": "#d2c000",
+				"700": "#a69700",
+				"800": "#796f00",
+				"900": "#4d4600",
+				"foreground": "#000",
+				"DEFAULT": "#ffe900"
+			  },
+			  "danger": {
+				"50": "#efdfdf",
+				"100": "#dab3b3",
+				"200": "#c48686",
+				"300": "#ae5959",
+				"400": "#982d2d",
+				"500": "#820000",
+				"600": "#6b0000",
+				"700": "#550000",
+				"800": "#3e0000",
+				"900": "#270000",
+				"foreground": "#fff",
+				"DEFAULT": "#820000"
+			  },
+			  "background": "#ffffff",
+			  "foreground": "#000000",
+			  "content1": {
+				"DEFAULT": "#ffffff",
+				"foreground": "#000"
+			  },
+			  "content2": {
+				"DEFAULT": "#f4f4f5",
+				"foreground": "#000"
+			  },
+			  "content3": {
+				"DEFAULT": "#e4e4e7",
+				"foreground": "#000"
+			  },
+			  "content4": {
+				"DEFAULT": "#d4d4d8",
+				"foreground": "#000"
+			  },
+			  "focus": "#006FEE",
+			  "overlay": "#000000"
+			}
+		  },
+		  "dark": {
+			"colors": {
+			  "default": {
+				"50": "#0d0d0e",
+				"100": "#19191c",
+				"200": "#26262a",
+				"300": "#323238",
+				"400": "#3f3f46",
+				"500": "#65656b",
+				"600": "#8c8c90",
+				"700": "#b2b2b5",
+				"800": "#d9d9da",
+				"900": "#ffffff",
+				"foreground": "#fff",
+				"DEFAULT": "#3f3f46"
+			  },
+			  "primary": {
+				"50": "#440b0b",
+				"100": "#6b1111",
+				"200": "#931717",
+				"300": "#ba1d1d",
+				"400": "#e22323",
+				"500": "#e74a4a",
+				"600": "#ec7070",
+				"700": "#f19797",
+				"800": "#f6bdbd",
+				"900": "#fbe4e4",
+				"foreground": "#fff",
+				"DEFAULT": "#e22323"
+			  },
+			  "secondary": {
+				"50": "#142a44",
+				"100": "#20436b",
+				"200": "#2c5c92",
+				"300": "#3774ba",
+				"400": "#438de1",
+				"500": "#64a1e6",
+				"600": "#85b5ec",
+				"700": "#a6c9f1",
+				"800": "#c7ddf6",
+				"900": "#e8f1fb",
+				"foreground": "#000",
+				"DEFAULT": "#438de1"
+			  },
+			  "success": {
+				"50": "#133b00",
+				"100": "#1d5d00",
+				"200": "#287f00",
+				"300": "#33a100",
+				"400": "#3ec300",
+				"500": "#60ce2d",
+				"600": "#82d859",
+				"700": "#a3e386",
+				"800": "#c5edb3",
+				"900": "#e7f8df",
+				"foreground": "#000",
+				"DEFAULT": "#3ec300"
+			  },
+			  "warning": {
+				"50": "#4d4600",
+				"100": "#796f00",
+				"200": "#a69700",
+				"300": "#d2c000",
+				"400": "#ffe900",
+				"500": "#ffed2d",
+				"600": "#fff159",
+				"700": "#fff586",
+				"800": "#fff8b3",
+				"900": "#fffcdf",
+				"foreground": "#000",
+				"DEFAULT": "#ffe900"
+			  },
+			  "danger": {
+				"50": "#270000",
+				"100": "#3e0000",
+				"200": "#550000",
+				"300": "#6b0000",
+				"400": "#820000",
+				"500": "#982d2d",
+				"600": "#ae5959",
+				"700": "#c48686",
+				"800": "#dab3b3",
+				"900": "#efdfdf",
+				"foreground": "#fff",
+				"DEFAULT": "#820000"
+			  },
+			  "background": "#000000",
+			  "foreground": "#ffffff",
+			  "content1": {
+				"DEFAULT": "#18181b",
+				"foreground": "#fff"
+			  },
+			  "content2": {
+				"DEFAULT": "#27272a",
+				"foreground": "#fff"
+			  },
+			  "content3": {
+				"DEFAULT": "#3f3f46",
+				"foreground": "#fff"
+			  },
+			  "content4": {
+				"DEFAULT": "#52525b",
+				"foreground": "#fff"
+			  },
+			  "focus": "#006FEE",
+			  "overlay": "#ffffff"
+			}
+		  }
+		},
+		"layout": {
+		  "disabledOpacity": "0.5"
+		}
+	  }), require("tailwindcss-animate")
   ],
 }
 
